4e0c6a2f29448917a3f74fb83c2140440466e07e
Marioespiro <maresprod5@alum.us.es>
2022-03-15 16:20:42 +0100

    Task 035: DONE

---
+++ src/main/java/acme/entities/patronageReports/PatronageReport.java
@@ -0,0 +1,62 @@
+package acme.entities.patronageReports;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.entities.patronages.Patronage;
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class PatronageReport extends AbstractEntity {
+	
+	// Serialisation identifier
+
+	protected static final long	serialVersionUID = 1L;
+
+	// Attributes
+		
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	protected Date creationMoment;
+	
+	@NotBlank
+	@Length(min=1, max=255)
+	protected String memorandum;
+	
+	@URL
+	protected String link;
+	
+	// Derived attributes
+	
+	@NotBlank
+	@Pattern(regexp = "〈[0-9]+〉:〈[0-9]{4}〉")
+	public String automaticSequenceNumber() {
+		return "〈" + this.getPatronage().getId() + "〉:〈" + this.getId() + "〉";
+	}
+	
+	// Relationships
+	
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Patronage patronage;
+	
+}
+
