dc59c4914e449e74cbcfc3f8e9446c843799e503
josemabp1 <josemabp13@gmail.com>
2022-03-15 16:19:32 +0100

    Task 034: Revision Done

--- src/main/java/acme/entities/patronages/Patronage.java
+++ src/main/java/acme/entities/patronages/Patronage.java
@@ -4,17 +4,23 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.PositiveOrZero;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
+import acme.framework.datatypes.Money;
 import acme.framework.entities.AbstractEntity;
+import acme.roles.Inventor;
+import acme.roles.Patron;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -33,6 +39,7 @@ public class Patronage extends AbstractEntity {
 	//@Enumerated(EnumType.STRING)
 	protected Status status;
 	
+	@NotBlank
 	@Column(unique = true)
 	@Pattern(regexp = "^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
 	protected String code;
@@ -43,14 +50,39 @@ public class Patronage extends AbstractEntity {
 	
 	@PositiveOrZero
 	@NotNull
-	protected Integer budget;
+	@Valid
+	protected Money budget;
 	
-	@NotNull
 	@Temporal(TemporalType.TIMESTAMP)
 	protected Date periodOfTime;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	protected Date creationTime;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	protected Date endingTime;
+	
+	@PositiveOrZero
+	@NotNull
+	@Valid
+	protected Money retailPrice;
 	
 	@URL
 	protected String link;
 	
 	// Relations -------------------------------------------------------------
+	
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Inventor inventor;
+	
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Patron patron;
 }
