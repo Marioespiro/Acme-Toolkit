70625a994957c4cbec75a15b9825ec7d2b2cb9cb
ezepersos <ezequielperez50@gmail.com>
2022-03-16 16:04:55 +0100

    Task 037 - Almost done
    Task done until the merge with master

---
+++ src/main/java/acme/dashboard/AdministratorDashboard.java
@@ -0,0 +1,38 @@
+
+package acme.dashboard;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdministratorDashboard implements Serializable {
+
+	// Serialisation Identifier 
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes
+	
+	Integer						totalItems;
+
+	Double						averageRetailPriceItems;
+	Double						deviationRetailPriceItems;
+	Integer						minRetailPriceItems;
+	Integer						maxRetailPriceItems;
+
+	Double						averageBudgetPatronages;
+	Double						deviationBudgetPatronages;
+	Integer						minBudgetPatronages;
+	Integer						maxBudgetPatronages;
+	
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+	
+	
+
+
+
+}
---
+++ src/main/java/acme/features/administrator/dashboard/AdministratorDashboardRepository.java
@@ -0,0 +1,50 @@
+
+package acme.features.administrator.dashboard;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.entities.items.ItemType;
+import acme.entities.patronages.Status;
+import acme.framework.repositories.AbstractRepository;
+
+@Repository
+public interface AdministratorDashboardRepository extends AbstractRepository {
+
+	@Query("select count(i) from Item i where i.itemType = :type")
+	Integer totalItems(Enum<ItemType> type);
+
+	@Query("select avg(i.retailPrice.amount) from Item i where i.itemType = :type")
+	Double averageRetailPriceItems(Enum<ItemType> type);
+
+	@Query("select stddev(i.retailPrice.amount) from Item i where i.itemType = :type group by i.technology, i.retailPrice.currency")
+	Double deviationRetailPriceItems(Enum<ItemType> type);
+
+	@Query("select min(i.retailPrice.amount) from Item i where i.itemType = :type group by i.technology, i.retailPrice.currency")
+	Double minRetailPriceItems(Enum<ItemType> type);
+
+	@Query("select max(i.retailPrice.amount) from Item i where i.itemType = :type group by i.technology, i.retailPrice.currency")
+	Double maxRetailPriceItems(Enum<ItemType> type);
+
+	@Query("select avg(datediff(t.budget, t.retailPrice)) from Patronage t where t.status = :type")
+	Double averageBugdetProposedPatronage(Enum<Status> type);
+	
+	@Query("select stddev(datediff(t.budget, t.retailPrice)) from Patronage t where t.status = :type")
+	Double deviationBugdetPatronage(Enum<Status> type);
+	
+	@Query("select min(datediff(t.budget,t.retailPrice)) from Patronage t where t.status = :type")
+	Double minimumBugdetPatronage(Enum<Status> type);
+	
+	@Query("select max(datediff(t.budget,t.retailPrice)) from Patronage t where t.status = :type")
+	Double maximumBugdetPatronage(Enum<Status> type);
+
+	/*
+	 * 
+	 * 
+	 * 
+	 * Double averageBudgetPatronages;
+	 * Double deviationBudgetPatronages;
+	 * Integer minBudgetPatronages;
+	 * Integer maxBudgetPatronages;
+	 */
+}
