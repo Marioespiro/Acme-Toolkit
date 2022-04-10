091c402b9aaa2f46536e742373a48ddec0296af6
Andrea Meca Sanchez <andmecsan@alum.us.es>
2022-04-10 19:31:11 +0200

    ficheros perdidos

--- src/main/java/acme/features/any/toolkit/AnyToolkitShowService.java
+++ src/main/java/acme/features/any/toolkit/AnyToolkitShowService.java
@@ -1,6 +1,54 @@
 package acme.features.any.toolkit;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class AnyToolkitShowService {
+import acme.entities.toolkit.Toolkit;
+import acme.framework.components.models.Model;
+import acme.framework.controllers.Request;
+import acme.framework.roles.Any;
+import acme.framework.services.AbstractShowService;
+
+@Service
+public class AnyToolkitShowService implements AbstractShowService<Any, Toolkit> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected AnyToolkitRepository repository;
+
+	// AbstractShowService<Administrator, Announcement> interface --------------
+
+
+	@Override
+	public boolean authorise(final Request<Toolkit> request) {
+		assert request != null;
+
+		return true;
+	}
+
+	@Override
+	public void unbind(final Request<Toolkit> request, final Toolkit entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		request.unbind(entity, model, "code", "title", "description", "assemblyNotes", "link");
+	}
+
+	@Override
+	public Toolkit findOne(final Request<Toolkit> request) {
+		assert request != null;
+
+		Toolkit result;
+		int id;
+
+		id = request.getModel().getInteger("id");
+		result = this.repository.findToolkitById(id);
+		
+
+		return result;
+	}
 
 }
+
