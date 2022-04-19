fdf2c91cfcff71c5f673547586fd663b80354d59
josemabp1 <josemabp13@gmail.com>
2022-04-19 20:29:53 +0200

    Task 067: Done

---
+++ src/test/java/acme/testing/inventor/patronageReport/InventorPatronageReportListAllTest.java
@@ -0,0 +1,35 @@
+package acme.testing.inventor.patronageReport;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class InventorPatronageReportListAllTest extends TestHarness { 
+	
+	// Lifecycle management ---------------------------------------------------
+
+		// Test cases -------------------------------------------------------------
+
+		@ParameterizedTest 
+		@CsvFileSource(resources = "/inventor/patronageReport/list-all.csv", encoding = "utf-8", numLinesToSkip = 1)
+		@Order(10)
+		public void positiveTest(final int recordIndex, final String creationMoment, final String memorandum, final String link, final String serialNumber) {
+			super.signIn("inventor1", "inventor1");
+			super.clickOnMenu("Inventor", "List patronage reports");
+			super.checkListingExists();
+			super.sortListing(0, "asc");
+
+			super.checkColumnHasValue(recordIndex, 0, serialNumber);
+			super.checkColumnHasValue(recordIndex, 1, creationMoment);
+			super.checkColumnHasValue(recordIndex, 2, memorandum);
+			super.checkColumnHasValue(recordIndex, 3, link);
+			//super.checkColumnHasValue(recordIndex, 4, patronage);
+
+			super.signOut();
+		}
+
+		// Ancillary methods ------------------------------------------------------
+
+}
